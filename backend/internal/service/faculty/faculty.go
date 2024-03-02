622ab4374f56ee878f78199f4e5e74b643afe33a
JoeManyak <doncrafttv@gmail.com>
2024-03-02 21:54:58 +0200

    add faculty)

---
+++ backend/internal/service/faculty/faculty.go
@@ -0,0 +1,37 @@
+package faculty
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"oss-backend/internal/models"
+)
+
+func (s *Service) GetFacultyByID(id uuid.UUID) (*models.Faculty, error) {
+	ctx := context.Background()
+
+	return s.repo.Faculty().GetFacultyByID(ctx, id)
+}
+
+func (s *Service) ListFaculties() ([]*models.Faculty, error) {
+	ctx := context.Background()
+
+	return s.repo.Faculty().ListFaculties(ctx)
+}
+
+func (s *Service) CreateFaculty(faculty *models.Faculty) error {
+	ctx := context.Background()
+
+	return s.repo.Faculty().CreateFaculty(ctx, faculty)
+}
+
+func (s *Service) UpdateFaculty(faculty *models.Faculty) error {
+	ctx := context.Background()
+
+	return s.repo.Faculty().UpdateFaculty(ctx, faculty)
+}
+
+func (s *Service) DeleteFaculty(id uuid.UUID) error {
+	ctx := context.Background()
+
+	return s.repo.Faculty().DeleteFaculty(ctx, id)
+}
---
+++ backend/internal/service/faculty/new.go
@@ -0,0 +1,13 @@
+package faculty
+
+import "oss-backend/internal/persistence"
+
+type Service struct {
+	repo persistence.Repo
+}
+
+func New(repo persistence.Repo) *Service {
+	return &Service{
+		repo: repo,
+	}
+}
